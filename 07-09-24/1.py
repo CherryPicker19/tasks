b9ed2e8958ab97b8fbc5c70fc549c7717993a4ed
CherryPicker19 <bashkatov.artm@gmail.com>
2025-01-25 20:56:53 +0300

    Update 1.py

--- 07-09-24/1.py
+++ 07-09-24/1.py
@@ -1,23 +1,22 @@
-s = input("Input: ")
-copys = s
-outs = ''
-povtors = ''
-indexFirst = 0
-indexLast = 0
-mass = []
+numb = int(input("Input: "))
+copyNumb = numb
+invNumb = 0
+first_dig = 0
+isOtr = False
+if numb < 0:
+    isOtr = True
+    numb = numb * -1
 
-for i in range(len(s)):
-    povtors += s[i]
-    #print(povtors)
-    if povtors.count(s[i]) <= 1:
-        outs += s[i]
-     #   print(outs)
-    else:
-        mass.append(outs)
-        outs = ''
-        indexLast = povtors.find(s[i])
-        povtors = ''
+while(True):
+    second_dig = numb % 10
+    numb = numb // 10
+    invNumb = invNumb * 10 + second_dig
 
-      #  print(indexLast)
-print(mass)
-
+    if numb == 0:
+        break
+if (invNumb < 127 or invNumb > -128) and isOtr == False:
+    print("Output: ", invNumb)
+if invNumb < 127 or invNumb > -128:
+    print("Output: ", invNumb * -1)
+else:
+    print("No solution")
