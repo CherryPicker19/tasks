10da47ae6d6621a28c1cf87a1d3f93548a2ffa4f
CherryPicker19 <bashkatov.artm@gmail.com>
2025-01-25 20:59:01 +0300

    Update 3.py

--- 07-09-24/3.py
+++ 07-09-24/3.py
@@ -1,34 +1,35 @@
-def is_skobki(s: str, acc: list, idx_last=0, longest=[], l=[]) -> bool:
-    if idx_last == len(s):
-        if acc == []:
-            return True
+word = input("Input word: ")
+val = int(input("Input int: "))
+schet = (val - 2) + val
+final = []
+
+for n in range(0, val):
+    for i in range(0, len(word), schet):
+        #print(n == val - 1 and i == len(word) - 1)
+        if n == val - 1 and i == len(word) - 1:
+            final.append(word[i - n])
+            break
+        if i == 0:
+            #print(word[n])
+            if n != val -1:
+                final.append(word[n])
+        elif i == len(word) - 1:
+            #print(word[ (-1 * n) -1])
+            if n != val - 1:
+                final.append(word[ (-1 * n) -1])
+        elif word[i - n] == word[i + n]:
+            #print(word[i + n])
+            if n != val - 1:
+                final.append(word[i + n])
+        elif word[i - n] == word[i]:
+            break
         else:
-            return max(l, default=False)
+            #print(word[i - n], word[i + n])
+            if n == val - 1 and i != 0:
+                final.append(word[i - n])
+            else:
+                final.append(word[i - n])
+                final.append(word[i + n])
 
-    acc.append(s[idx_last])
-    if len(acc) == 1:
-        return is_skobki(s, acc, idx_last+1)
-
-    if acc[-2] in open_brackets and acc[-1] in close_brackets:
-        if couples.get(acc[-2]) == couples.get(acc[-1]):
-            longest.insert(0, acc[-2])
-            longest.append(acc[-1])
-            acc.pop()
-            acc.pop()
-            return is_skobki(s, acc, idx_last+1)
-    if longest != []:
-        a = ''.join(longest)
-        l.append(a)
-        longest.clear()
-    return is_skobki(s, acc, idx_last+1)#
-
-def main():
-    global open_brackets, close_brackets, couples
-    open_brackets = {"(", "{", "["}
-    close_brackets = {")", "}", "]"}
-    couples = {"(": 1, ")": 1, "{": 2, "}": 2, "[": 3, "]": 3}
-    s = "{[(]){[()]}}"
-    print(is_skobki(s, []))
-
-if __name__ == '__main__':
-    main()
+fin_string = ''.join(final)
+print(fin_string)
